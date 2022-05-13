3c426e7d32b8774c2d7e41605a263977116adb5b
Reganto <tell.reganto@gmail.com>
2022-05-13 16:10:50 +0430

    Fixed #7 -- Added permissions to views
    Follow up 596c7ea

--- backend/api/v4/views.py
+++ backend/api/v4/views.py
@@ -1,14 +1,52 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from api.v4.serializers import PostSerializer, CommentSerializer
 from api.models import Post, Comment
+from api.permissions import (
+    IsStaffOrReadOnly,
+    IsSuperUserOrAuthorOrReadOnly,
+    IsSuperUserOrOwnerOrReadOnly,
+)
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def get_permissions(self):
+        permission_classes = [
+            IsAuthenticated,
+            IsStaffOrReadOnly,
+        ]
+        if self.action in [
+            "update",
+            "partial_update",
+            "destroy",
+        ]:
+            permission_classes = [
+                IsAuthenticated,
+                IsSuperUserOrAuthorOrReadOnly,
+            ]
+        return [permission() for permission in permission_classes]
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def get_permissions(self):
+        permission_classes = [
+            IsAuthenticated,
+            IsStaffOrReadOnly,
+        ]
+        if self.action in [
+            "update",
+            "partial_update",
+            "destroy",
+        ]:
+            permission_classes = [
+                IsAuthenticated,
+                IsSuperUserOrOwnerOrReadOnly,
+            ]
+        return [permission() for permission in permission_classes]
